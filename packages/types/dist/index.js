f0e37bafb34634c2de787648728fe80cf826613d
Ankish Nayak <ankish2000nayak@gmail.com>
2023-09-26 16:53:02 +0530

    added types

---
+++ packages/types/dist/index.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.questionTypes = exports.teacherLoginTypes = exports.studentLoginTypes = exports.studentSignupTypes = exports.teacherSignupTypes = void 0;
+const zod_1 = require("zod");
+exports.teacherSignupTypes = zod_1.z.object({
+    firstname: zod_1.z.string().min(1),
+    lastname: zod_1.z.string().min(1),
+    username: zod_1.z.string().email(),
+    password: zod_1.z.string().min(8),
+});
+exports.studentSignupTypes = zod_1.z.object({
+    firstname: zod_1.z.string().min(1),
+    lastname: zod_1.z.string().min(1),
+    username: zod_1.z.string().email(),
+    password: zod_1.z.string().min(8),
+});
+exports.studentLoginTypes = zod_1.z.object({
+    username: zod_1.z.string().email(),
+    password: zod_1.z.string().min(8),
+});
+exports.teacherLoginTypes = zod_1.z.object({
+    username: zod_1.z.string().email(),
+    password: zod_1.z.string().min(8),
+});
+exports.questionTypes = zod_1.z.object({
+    title: zod_1.z.string().min(1),
+    description: zod_1.z.string().max(100),
+    question: zod_1.z.string().min(1),
+    option1: zod_1.z.string().min(1),
+    option2: zod_1.z.string().min(1),
+    option3: zod_1.z.string().min(1),
+    option4: zod_1.z.string().min(1),
+    answer: zod_1.z.string().min(1),
+});
---
+++ packages/types/src/index.ts
@@ -0,0 +1,37 @@
+import { z } from "zod";
+export const teacherSignupTypes = z.object({
+  firstname: z.string().min(1),
+  lastname: z.string().min(1),
+  username: z.string().email(),
+  password: z.string().min(8),
+});
+export const studentSignupTypes = z.object({
+  firstname: z.string().min(1),
+  lastname: z.string().min(1),
+  username: z.string().email(),
+  password: z.string().min(8),
+});
+export const studentLoginTypes = z.object({
+  username: z.string().email(),
+  password: z.string().min(8),
+});
+export const teacherLoginTypes = z.object({
+  username: z.string().email(),
+  password: z.string().min(8),
+});
+export const questionTypes = z.object({
+  title: z.string().min(1),
+  description: z.string().max(100),
+  question: z.string().min(1),
+  option1: z.string().min(1),
+  option2: z.string().min(1),
+  option3: z.string().min(1),
+  option4: z.string().min(1),
+  answer: z.string().min(1),
+});
+
+export type questionParams = z.infer<typeof questionTypes>;
+export type studentSignupParams = z.infer<typeof studentSignupTypes>;
+export type teacherSignupParams = z.infer<typeof teacherSignupTypes>;
+export type studentLoginParams = z.infer<typeof studentLoginTypes>;
+export type teacherLoginTypes = z.infer<typeof teacherLoginTypes>;
