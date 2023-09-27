8486044c9588b12723a99bcc0e6aa0a55546af30
Ankish Nayak <ankish2000nayak@gmail.com>
2023-09-27 09:51:38 +0530

    implemented teacher routes to CRUD on questions

--- apps/server/src/routes/teacher.ts
+++ apps/server/src/routes/teacher.ts
@@ -112,12 +112,142 @@ router.post(
         console.log(e);
         res.status(404).json({ message: "db error" });
       }
-    }else{
-        res.status(404).json({message: "Teacher does not exists"});
+    } else {
+      res.status(404).json({ message: "Teacher does not exists" });
     }
   }
 );
 // update question route
-router.post('/questions/:questionId', authenticateJwt, async ( req: Request, res: Response) => {
+router.put(
+  "/questions/:questionId",
+  authenticateJwt,
+  async (req: Request, res: Response) => {
+    const parsedInput = questionTypes.safeParse(req.body);
+    if (!parsedInput.success) {
+      return res.status(411).json({ error: parsedInput.error });
+    }
+    if (typeof req.headers["teacher"] === "string") {
+      const questionData: questionParams = parsedInput.data;
+      const questionId = parseInt(req.params.questionId);
+      const username: string = req.headers["teacher"] || "";
+      try {
+        const teacher = await prisma.teacher.findUnique({
+          where: {
+            username,
+          },
+        });
+        if (teacher) {
+          const updatedQuestion = await prisma.question.update({
+            where: {
+              id: questionId,
+            },
+            data: {
+              ...questionData,
+            },
+          });
+          if (updatedQuestion) {
+            res.json({ message: "Question upated", questionId });
+          } else {
+            res.status(403).json({ message: "Failed to updated question" });
+          }
+        } else {
+          res.status(403).json({ message: "Teacher dose not exists" });
+        }
+      } catch (e) {
+        console.log(e);
+        res.status(404).json({ message: "db error" });
+      }
+    } else {
+      res.status(403).json({ message: "Teacher dose not exists" });
+    }
+  }
+);
 
-})
+// get all questions from all teachers
+router.get(
+  "/questions",
+  authenticateJwt,
+  async (req: Request, res: Response) => {
+    if (req.headers["teacher"] === "string") {
+      const username: string = req.headers["teacher"];
+      try {
+        const teacher = await prisma.teacher.findUnique({
+          where: {
+            username,
+          },
+        });
+        if (teacher) {
+          const questions = await prisma.question.findMany({});
+          res.json({ questions });
+        } else {
+          return res.status(403).json({ message: "Teacher dose not exists" });
+        }
+      } catch (e) {
+        console.log(e);
+        res.status(404).json({ message: "db error" });
+      }
+    } else {
+      res.status(403).json({ message: "Teacher dose not exists" });
+    }
+  }
+);
+
+// get questions from logged in teacher
+router.get(
+  "/questions/me",
+  authenticateJwt,
+  async (req: Request, res: Response) => {
+    if (req.headers["teacher"] === "string") {
+      const username: string = req.headers["teacher"];
+      try {
+        const teacher = await prisma.teacher.findUnique({
+          where: {
+            username,
+          },
+          include: {
+            questions: true,
+          },
+        });
+        if (teacher) {
+          res.json({ questions: teacher.questions });
+        } else {
+          return res.status(403).json({ message: "Teacher dose not exists" });
+        }
+      } catch (e) {
+        console.log(e);
+        res.status(404).json({ message: "db error" });
+      }
+    } else {
+      res.status(403).json({ message: "Teacher dose not exists" });
+    }
+  }
+);
+
+// get questions from particular given id teacher
+router.get(
+  "/questions/:teacherId",
+  authenticateJwt,
+  async (req: Request, res: Response) => {
+    if (req.headers["teacher"] === "string") {
+      const username: string = req.headers["teacher"];
+      const teacherId = parseInt(req.params.teacherId);
+      try {
+        const teacher = await prisma.teacher.findUnique({
+          where: { id: teacherId },
+          include: { questions: true },
+        });
+        if (teacher) {
+          res.json({ questions: teacher.questions });
+        } else {
+          res.status(403).json({ message: "Teacher dose not exists" });
+        }
+      } catch (e) {
+        console.log(e);
+        res.status(404).json({ message: "db error" });
+      }
+    } else {
+      res.status(403).json({ message: "Teacher dose not exists" });
+    }
+  }
+);
+
