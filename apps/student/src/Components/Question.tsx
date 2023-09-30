bcd466636de43dab66473a3f93f05439f88ce078
Ankish Nayak <ankish2000nayak@gmail.com>
2023-09-30 16:36:31 +0530

    updated Card and conditional rendering of add button

---
+++ apps/student/src/Components/Question.tsx
@@ -0,0 +1,197 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { BASE_URL } from "../config";
+import { questionParams } from "types";
+import {
+  Grid,
+  Card,
+  Typography,
+  Button,
+  CardContent,
+  CardActions,
+} from "@mui/material";
+import { useRecoilState } from "recoil";
+import { questionCart } from "../store/atoms/questionCart";
+type question = questionParams & { id: Number } & { creatorId: Number };
+export const Question = () => {
+  const { questionId } = useParams();
+  const [question, setQuestion] = useState<question>();
+  const [add, setAdd] = useState<Boolean>(false);
+  const [testQuestions, setTestQuestions] = useRecoilState(questionCart);
+  const init1 = async (): Promise<question> => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const response = await axios.get(
+          `${BASE_URL}/teacher/questions/${questionId}`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const data = response.data;
+        if (data.question) {
+          setQuestion(data.question);
+          resolve(data.question);
+        }
+      } catch (e) {
+        const errObj = {
+          msg: "An error occurred",
+          err: e,
+        };
+        console.log(errObj);
+        reject(errObj);
+      }
+    });
+  };
+  const init2 = async (question: question) => {
+    if (
+      testQuestions.questions &&
+      testQuestions.questions.find((e) => e.id === question.id)
+    ) {
+      setAdd(true);
+    } else {
+      setAdd(false);
+    }
+    try {
+      const response = await axios.get(`${BASE_URL}/teacher/questions/me`, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = response.data;
+      if (data.questions) {
+        const questions = data.questions;
+        console.log("creatorId", question.creatorId);
+        console.log(questions);
+        for (let i = 0; i < questions.length; ++i) {
+          if (questions[i].id === question.id) {
+            break;
+          }
+        }
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    init1().then((question) => {
+      init2(question);
+    });
+  }, []);
+  if (!question) {
+    return <>404</>;
+  }
+  return (
+    <Grid
+      container
+      style={{
+        padding: "10vh 10vw",
+      }}
+    >
+      <Grid
+        item
+        xl={6}
+        style={{
+          padding: " 0 3vw",
+        }}
+      >
+        <img
+          src="/question-mark.webp"
+          style={{
+            maxWidth: "100%",
+            maxHeight: "100%",
+            // padding: 'auto'
+          }}
+        />
+      </Grid>
+      <Grid
+        item
+        xl={6}
+        style={{
+          padding: "0 4vw",
+        }}
+      >
+        <Card
+          variant="outlined"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            border: "1px solid black",
+          }}
+        >
+          <CardContent>
+            <Typography variant="h4">Title: {question.title}</Typography>
+            <Typography variant="body1">
+              Description: {question.description}
+            </Typography>
+            <Typography variant="h5">Question: {question.question}</Typography>
+
+            <div
+              style={
+                {
+                  // display: "flex",
+                  // flexDirection: "row",
+                }
+              }
+            >
+              <Typography variant="h6">{"Options:"}</Typography>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-evenly",
+                  alignItems: "center",
+                }}
+              >
+                <Typography variant={"h6"}>{question.option1}</Typography>
+                <Typography variant={"h6"}>{question.option2}</Typography>
+                <Typography variant={"h6"}>{question.option3}</Typography>
+                <Typography variant={"h6"}>{question.option4}</Typography>
+              </div>
+            </div>
+            {/* <Typography variant={"h6"}>Answer: {question.answer}</Typography> */}
+          </CardContent>
+          <CardActions>
+            <Button
+              variant="contained"
+              onClick={() => {
+                setAdd((add) => {
+                  return !add;
+                });
+                if (add) {
+                  const newTestQuestions: { id: Number }[] = [];
+                  if (testQuestions.questions)
+                    testQuestions.questions.forEach((element) => {
+                      if (question.id !== element.id)
+                        newTestQuestions.push(element);
+                    });
+                  setTestQuestions({
+                    isLoading: false,
+                    questions: newTestQuestions,
+                  });
+                } else {
+                  const newTestQuestions: { id: Number }[] = [];
+                  if (testQuestions.questions)
+                    testQuestions.questions.forEach((element) => {
+                      newTestQuestions.push(element);
+                    });
+                  newTestQuestions.push({ id: question.id });
+                  setTestQuestions({
+                    isLoading: false,
+                    questions: newTestQuestions,
+                  });
+                }
+              }}
+            >
+              {add ? "remove" : "add"}
+            </Button>
+          </CardActions>
+        </Card>
+      </Grid>
+    </Grid>
+  );
+};
