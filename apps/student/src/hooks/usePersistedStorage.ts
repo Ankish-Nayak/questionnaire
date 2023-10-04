0c54b3fdb0b763e10c41cab6428a5cc470be8b57
Ankish Nayak <ankish2000nayak@gmail.com>
2023-10-04 13:33:17 +0530

    updated timer atom and made it persistent

--- apps/student/src/hooks/usePersistedStorage.ts
+++ apps/student/src/hooks/usePersistedStorage.ts
@@ -1,5 +1,10 @@
-import { MutableSnapshot, useRecoilTransactionObserver_UNSTABLE } from "recoil";
+import {
+  MutableSnapshot,
+  snapshot_UNSTABLE,
+  useRecoilTransactionObserver_UNSTABLE,
+} from "recoil";
 import { questionCart } from "../store/atoms/questionCart";
+import { timer } from "../store/atoms/timer";
 
 // Serializing data in Recoil is an unstable feature, but it's still available in the published version. useRecoilTransactionObserver_UNSTABLE is a named export of the recoil package, and calls a function every time any Recoil state is changed.
 export const usePersistStorage = () => {
@@ -13,24 +18,41 @@ export const usePersistStorage = () => {
 //   processSnapshot(snapshot);
 // });
 // @ts-ignore
-const processSnapshot = async (snapshot) => {
+const generateData = async (snapshot) => {
   const persistedQuestion = await snapshot.getPromise(questionCart);
-  localStorage.setItem(
-    "question_cart",
-    JSON.stringify({
-      testQuestions: persistedQuestion,
-    })
-  );
+  const persistedTimer = await snapshot.getPromise(timer);
+  return {
+    testQuestion: persistedQuestion,
+    timer: persistedTimer,
+  };
+};
+const processSnapshot = async (snapshot: any) => {
+  const data = await generateData(snapshot);
+  localStorage.setItem("questionnaire", JSON.stringify(data));
 };
 export const initState = (snapshot: MutableSnapshot) => {
-  const data = localStorage.getItem("question_cart");
-  if (!data) return;
-  const { testQuestions: persistedQuestion } = JSON.parse(data);
-  if (!persistedQuestion || !persistedQuestion.questions) return;
-  snapshot.set(questionCart, {
-    isLoading: false,
-    questions: persistedQuestion.questions.map((questionId: Number) => {
-      return questionId;
-    }),
-  });
+  const data = localStorage.getItem("questionnaire");
+  if (!data) {
+    return;
+  }
+  const outputData = JSON.parse(data);
+  setData(outputData, snapshot.set);
+};
+
+export const setData = (data: any, set: MutableSnapshot["set"]) => {
+  const { testQuestion: persistedQuestion, timer: persistedTimer } = data;
+  if (persistedQuestion && persistedQuestion.questions)
+    set(questionCart, {
+      isLoading: false,
+      questions: persistedQuestion.questions.map((questionId: number) => {
+        return questionId;
+      }),
+    });
+  if (persistedTimer && persistedTimer.show)
+    set(timer, {
+      isLoading: false,
+      show: persistedTimer.show,
+      startTime: persistedTimer.startTime,
+      endTime: persistedTimer.endTime,
+    });
 };
--- apps/student/src/store/atoms/timer.ts
+++ apps/student/src/store/atoms/timer.ts
@@ -1,9 +1,26 @@
 import { atom } from "recoil";
 
-export const timer = atom({
+export const timer = atom<{
+  isLoading: boolean;
+  show: boolean;
+  startTime: number;
+  endTime: number;
+}>({
   key: "timer",
   default: {
     isLoading: true,
     show: false,
+    startTime: 0,
+    endTime: 0,
   },
+});
+
+export const timeOut = atom<NodeJS.Timeout | undefined>({
+  key: "timeOut",
+  default: undefined,
+});
+
+export const timeInterval = atom<NodeJS.Timer | undefined>({
+  key: "timeInterval",
+  default: undefined,
 });
