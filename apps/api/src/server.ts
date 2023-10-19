6897fe82aad8ceed879ccb1f1ab3f36c66371584
Ankish Nayak <ankish2000nayak@gmail.com>
2023-10-19 10:08:11 +0530

    dropping tsoa

--- apps/api/src/server.ts
+++
@@ -1,8 +0,0 @@
-import { app } from "./app";
-import errorHandler from 'api-error-handler';
-const port = process.env.PORT || 3000;
-
-app.use(errorHandler());
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
